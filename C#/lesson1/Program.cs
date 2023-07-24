2cfdf44ca55bbb6fd8f6637977888193bb3606bb
tamarakokhan <tamara.n.kokhan@gmail.com>
2023-07-24 20:15:01 +0300

    добавила папку по с# с первыми лекцией и семинаром

---
+++ C#/lesson1/Program.cs
@@ -0,0 +1,110 @@
+﻿void Ex1_HelloConsole()
+{
+    Console.WriteLine(" 2 Hello, World! ");
+}
+
+void Ex2_HelloUser()
+{
+    Console.Write("Введите Ваше имя.");
+    string username = Console.ReadLine();
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
+
+void Ex3_sum()
+{
+    int numberA = new Random().Next(1, 10); // Случаное целое число от 1 до 10 (1, 2, 3..., 9)
+    Console.WriteLine(numberA);
+    int numberB = new Random().Next(1, 10);
+    Console.WriteLine(numberB);
+    int result = numberA + numberB;
+    Console.WriteLine(result);
+}
+
+void Ex4_div()
+{
+    double numberA = 12;
+    double numberB = 5;
+    Console.WriteLine(numberA / numberB);
+}
+
+void Ex5_IfElse()
+{
+    Console.Write("Введите имя пользователя: ");
+    string username = Console.ReadLine();
+
+    if (username.ToLower() == "маша")
+    {
+        Console.WriteLine("Ура, это же МАША!");
+    }
+    else
+    {
+        Console.Write("Привет, ");
+        Console.WriteLine(username);
+    }
+}
+
+void Ex6_base()
+{
+    int a = 1;
+    int b = 2;
+    int c = 6;
+    int d = 8;
+    int e = 4;
+
+    int max = a;
+
+    if (b > max) max = b;
+    if (c > max) max = c;
+    if (d > max) max = d;
+    if (e > max) max = e;
+
+    Console.Write("max = ");
+    Console.WriteLine(max);
+}
+
+void Ex7_magic()
+{
+    Console.Clear();
+    
+    int xa = 40, ya = 1,
+        xb = 1, yb = 30,
+        xc = 80, yc = 30;
+
+    Console.SetCursorPosition(xa, ya);
+    Console.WriteLine("+");
+
+    Console.SetCursorPosition(xb, yb);
+    Console.WriteLine("+");
+
+    Console.SetCursorPosition(xc, yc);
+    Console.WriteLine("+");
+
+    int x = xa, y = xb;
+    int count = 0;
+
+    while (count < 10000)
+    {
+        int what = new Random().Next(0, 3);
+        if (what == 0)
+        {
+            x = (x + xa) / 2;
+            y = (y + ya) / 2;
+        }
+        if (what == 1)
+        {
+            x = (x + xb) / 2;
+            y = (y + yb) / 2;
+        }
+        if (what == 2)
+        {
+            x = (x + xc) / 2;
+            y = (y + yc) / 2;
+        }
+
+        Console.SetCursorPosition(x, y);
+        Console.WriteLine("+");
+
+        count++;
+    }
+}
---
+++ C#/seminar1/Program.cs
@@ -0,0 +1,132 @@
+﻿void Ex1()
+{
+    // Напишите программу, которая на вход
+    // принимает число и выдаёт его квадрат (число
+    // умноженное на само себя).
+
+    Console.WriteLine("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(num * num);
+}
+
+void Ex2()
+{
+    // Напишите программу, которая на вход
+    // принимает число и выдаёт его квадрат (число
+    // умноженное на само себя).
+
+    Console.WriteLine("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(num * num);
+}
+
+void Ex3()
+{
+    // Напишите программу, которая на вход принимает два
+    // числа и проверяет, является ли первое число квадратом
+    // второго.
+
+    Console.WriteLine("Введите 1-ое число: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите 2-ое число: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    if (num2 == num1 * num1)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+
+void Ex4()
+{
+    // Напишите программу, которая будет выдавать
+    // название дня недели по заданному номеру.
+
+    // Вариант 1
+    Console.WriteLine("Введите число: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    if (num1 > 7)
+    {
+        Console.WriteLine("Нет такого дня недели.");
+    }
+    else if (num1 == 1)
+    {
+        Console.WriteLine("Понедельник");
+    }
+    else if (num1 == 2)
+    {
+        Console.WriteLine("Вторник");
+    }
+    else if (num1 == 3)
+    {
+        Console.WriteLine("Среда");
+    }
+    else if (num1 == 4)
+    {
+        Console.WriteLine("Четверг");
+    }
+    else if (num1 == 5)
+    {
+        Console.WriteLine("Пятница");
+    }
+    else if (num1 == 6)
+    {
+        Console.WriteLine("Суббота");
+    }
+    else if (num1 == 7)
+    {
+        Console.WriteLine("Воскресенье");
+    }
+
+    // Вариант 2
+    var days = new Dictionary<int, string>()
+{
+    { 1, "Понедельник"},
+    { 2, "Вторник"},
+    { 3, "Среда"},
+    { 4, "Четверг"},
+    { 5, "Пятница"},
+    { 6, "Суббота"},
+    { 7, "Воскресенье"},
+};
+    Console.WriteLine("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num > 0 & num < 8)
+        Console.WriteLine(days[num]);
+    if (num > 7)
+    {
+        Console.WriteLine("Нет такого дня недели.");
+    }
+}
+
+void Ex5()
+{
+    // Напишите программу вычисления значения
+    // функции возведения числа в квадрат.
+
+    int Square(int x) => x * x;
+
+    Console.WriteLine("Введите число: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(Square(x));
+}
+
+void Ex6()
+{
+    // Напишите программу, которая на вход принимает одно число
+    // (N), а на выходе показывает все целые числа в промежутке от -N
+    // до N.
+
+    Console.WriteLine("Введите число: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+
+    for (int i = -x; i <= x; i++)
+    {
+        Console.Write(i + " ");
+    }
+}
